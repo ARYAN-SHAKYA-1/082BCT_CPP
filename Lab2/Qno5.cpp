543680c3fef23676c8de2595af0eb3b60dc4f9c2
aryan_shakya <shakyaaryan567@gmail.com>
2026-05-26 16:13:31 +0545

    qno5 aryan -2nd

---
+++ Lab2/Qno5.cpp
@@ -0,0 +1,14 @@
+// WAP  in C++ to find the TSA, CSA and Volume of  
+// cylinder using the concept of class and object.
+
+#include<iostream>
+using namespace std;
+
+class Shapes
+{
+    int length, radius;
+    public:
+    void input(){
+        cin>>length>>radius;
+    }
+};
