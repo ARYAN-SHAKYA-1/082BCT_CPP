c018236ad63a7e83fff8a112967632c845092a15
Arjan <arjanshakya77@gamil.com>
2026-06-02 16:35:35 +0545

    mero project

---
+++ Lab3/Qno5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class test
+{
+    int a;
+    public:
+        void input()
+        {
+            cout<<"enter value";
+            cin>>a;
+        }
+        void operator --(int)
+        {
+            a=a-5;//plus 5 huncha.
+        }
+        void dispay()
+        {
+            cout<<"the value of a:"<<a;
+        }
+};
+
+int main()
+{
+    test t;
+    t.input();
+    t--;
+    t.dispay();
+}
