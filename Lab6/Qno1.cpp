1a367408308fa29bad8b5b9126020d79aa1137b9
aryan_shakya <shakyaaryan567@gmail.com>
2026-07-07 15:33:42 +0545

    qno 1 and qno 2

--- Lab6/Qno1.cpp
+++ Lab6/Qno1.cpp
@@ -1,4 +1,31 @@
 /*
-wap to create a base class rectangle 
+Qno1
+wap to create a base class rectange that takes input (lenght, breadth).Derive a child class claculate ot find the area and perimeter of rectange
 */
 #include <iostream>
+using namespace std;
+
+class Rectangle{
+    protected:
+    int lenght , breadth;
+    public:
+    void input(){
+        cout<<"enter lenght and breadth:";
+        cin>>lenght>>breadth;
+    }
+};
+class calculate : public Rectangle{
+    public:
+    int area(){
+        return (lenght * breadth);
+    }
+    int preimeter(){
+        return (2*(lenght+breadth));
+    }
+    
+};
+int main(){
+    calculate calc1;
+    calc1.input();
+    cout<< "area:" << calc1.area()<< endl<< "perimeter: "<<calc1.preimeter()<< endl;
+}
