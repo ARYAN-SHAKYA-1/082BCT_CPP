cef1bebacde5316f4d0167e29d7a10cb2c0d05c3
aryan_shakya <shakyaaryan567@gmail.com>
2026-08-07 09:28:29 +0545

    class file created and teacher file created

---
+++ project/class.cpp
@@ -0,0 +1,12 @@
+//this file will be house all the classes that are going to be used in this program.
+
+
+//person class the base class. 
+class person{
+ protected:
+    char name[20], address[20];
+    int age,id;
+};
+
+
+//
