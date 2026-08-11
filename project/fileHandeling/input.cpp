71197982acd2a4c94bebf28e82600603a4d52474
aryan_shakya <shakyaaryan567@gmail.com>
2026-08-11 10:40:54 +0545

    starting to make filehandeling part

---
+++ project/fileHandeling/input.cpp
@@ -0,0 +1,12 @@
+// #include "../classes/student.cpp"
+// #include "../classes/teacher.cpp"
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+using namespace std;
+
+void input(){
+    system("clear");
+
+    cout<<"input screen";
+}
